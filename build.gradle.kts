733b0f2691593d32718ba92fec66d4c1fc6c6064
iserveu_siba <shibananda.m@iserveu.in>
2023-09-28 15:30:55 +0530

    Completely Stabilized

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ tasks {
     create("stage").dependsOn("installDist")
 }
 
+
 repositories {
     mavenCentral()
     google()
--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -5,16 +5,14 @@ import com.example.plugins.routes.homeRoutes
 import com.example.plugins.routes.getMethodRoutes
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.routing.*
 
-fun main(){
-    embeddedServer(factory = Netty, port = 8080, host = "0.0.0.0", module = Application::applicationModule).start(true)
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
 }
 
-fun Application.applicationModule() {
+fun Application.module() {
     /**
      * below code used for install ContentNegotiation for Serializable of json object
      */
